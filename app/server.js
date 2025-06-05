15e7de64005b31fc374135169076caeb24f5e7da
Parham <161451448+Parhamazs@users.noreply.github.com>
2025-06-05 13:46:17 +0000

    redis installed and update server.js code for appending redis

--- app/server.js
+++ app/server.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const http = require('http')
-const socketIO = require('socket.io')
+const { server } = require('socket.io')
+const redis = require('redis');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = new server(server);
+
+const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST || 'localhost'}:6379`
+});
+
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.connect();
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('a user connected now');
-    socket.on('chat message', (msg) => {
+
+    redisClient.IRange('messages', 0, -1).then((messages) =>{
+        messages.forEach((msg) => socket.emit('chat message', msg));
+    });
+
+    socket.on('chat message', async (msg) => {
+        await redisClient.rPush('messages', msg);
         io.emit('chat mesage', msg);
     });
 
